a502c4a32c8bdba53af635ade77f3ba62a491484
Prafful Agrawal <prafful026@gmail.com>
2022-04-15 01:07:55 +0530

    chore: refactored

--- lost-n-found-backend/configs/database.go
+++ lost-n-found-backend/configs/database.go
@@ -10,40 +10,39 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func ConnectDB() *mongo.Client {
 
-func ConnectDB() *mongo.Client{
+	client, err := mongo.NewClient(options.Client().ApplyURI(GetEnvVar(mongoUriKey)))
 
-	client,err:=mongo.NewClient(options.Client().ApplyURI(getEnvVar(mongoUriKey)))
-
-	if err!=nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
-    ctx,_:=context.WithTimeout(context.Background(),connectionTime*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), connectionTime*time.Second)
 
 	err = client.Connect(ctx)
 
-	if err!=nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
-    err = client.Ping(ctx, nil)
+	err = client.Ping(ctx, nil)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Println("Connected to MongoDB")
+	fmt.Println("Connected to MongoDB")
 
-    return client
+	return client
 }
 
-func DisconnectDB(client *mongo.Client){
-    ctx,_:=context.WithTimeout(context.Background(),connectionTime*time.Second)
-    client.Disconnect(ctx)
+func DisconnectDB(client *mongo.Client) {
+	ctx, _ := context.WithTimeout(context.Background(), connectionTime*time.Second)
+	client.Disconnect(ctx)
 }
 
-func GetCollections(client *mongo.Client, collectionName string) *mongo.Collection{
-	collections:=client.Database("lost-n-found").Collection(collectionName)
+func GetCollections(client *mongo.Client, collectionName string) *mongo.Collection {
+	collections := client.Database("lost-n-found").Collection(collectionName)
 	return collections
 }
